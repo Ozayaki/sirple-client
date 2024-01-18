6dc85c6746a185a94ded3b487abe742b294acd23
Ozayaki <156956569+Ozayaki@users.noreply.github.com>
2024-01-18 16:55:29 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var inp = document.getElementById('code');
+const but = document.getElementById('but');
+const txt = document.getElementById('text');
+const g = document.getElementById('ground');
+const l = document.getElementById('layer');
+but.onclick = function () {
+    if (inp.value == "free") {
+         l.style.visibility = "hidden";
+         document.body.style.backgroundImage = "url('./imgs/scream.png')";
+         var audio = document.getElementById('aud')
+         audio.play();
+    }else {
+        txt.textContent = "Офигел?";
+        setTimeout(() => {
+            txt.textContent = "Оставить жалобу" 
+        }, 2000)
+    }
+}
